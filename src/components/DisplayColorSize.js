40c4b90e544badacde16f336d9b81e2500ca717a
vuongvgc <vuongvgc@gmail.com>
2020-10-11 21:46:16 +0700

    build display color

--- src/components/DisplayColorSize.js
+++ src/components/DisplayColorSize.js
@@ -1,9 +1,15 @@
 import React from 'react';
 function DisplayColorSize(props) {
+    const styleEl = {
+        fontSize: props.fontSize,
+        border: '3px solid',
+        borderColor: 'props.colorPick',
+        color: props.colorPick
+    }
     return(
         <div className="container">
             <h3>Color: {props.colorPick}  - FontSize: {props.fontSize} px</h3>
-            <div className="border border-danger">
+            <div style={styleEl}>
                 <p>Please fill input</p>
             </div>
         </div>
